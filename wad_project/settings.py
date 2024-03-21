9a3133836e75b137ab37152ebd6bb3e12e240620
“Davit <2840850M@student.gla.ac.uk>
2024-03-21 16:13:10 +0000

    Experiment 8 PythonAnywhere

--- wad_project/settings.py
+++ wad_project/settings.py
@@ -40,7 +40,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATIC_ROOT = '/home/foodieshare/WAD-Project-FoodieShare'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 
 
